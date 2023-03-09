e404f12f86ceceef7f16f906dcc480d86b86909e
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2023-03-09 15:34:17 +0100

    First version mostly working

--- tp2/exercice1.jl
+++ tp2/exercice1.jl
@@ -18,22 +18,23 @@ function subProblem(y_val::Matrix{Float64}, bnd::Int64)::Any
     @variable(sub_model, v[i in 1:n] >= 0.)
 
     ##### Objective #####
-    @objective(sub_model, Max, - bnd*sum(y_val[i,j] * v_ij[i,j] for i in 1:n for j in 1:n if adj[i,j] > 0.0) + 
+    @objective(sub_model, Max, - bnd*sum(y_val[i,j] * v_ij[i,j] for i in 1:n for j in 1:n if adj[i,j] > 0.0 && i < j ) + 
                 sum(demande[i] * v[i] for i in 1:n))
     
     ##### Constraints #####
     @constraint(sub_model, v[1] == 0)
     # Feasibility constraint
-    @constraint(sub_model, sum(v_ij[i,j] for i in 1:n for j in 1:n if adj[i,j] > 0.0) == 1)
-    @constraint(sub_model, sum(v[i] for i in 1:n)== 1)
+    @constraint(sub_model, sum(v_ij[i,j] for i in 1:n for j in 1:n if adj[i,j] > 0.0 && i < j) + sum(v[i] for i in 1:n) == 1)
+    #@constraint(sub_model, sum(v_ij[i,j] for i in 1:n for j in 1:n if adj[i,j] > 0.0 && i < j) == 1)
+    #@constraint(sub_model, sum(v[i] for i in 1:n)== 1)
 
 
     # Edges constraint
     for i in 1:n
         for j in 1:n
-            if adj[i,j] > 0.0
-                @constraint(sub_model, v_ij[i,j] - v[i] + v[j] <= 0)
-                @constraint(sub_model, v_ij[i,j] + v[i] - v[j] <= 0)
+            if adj[i,j] > 0.0 && i < j
+                @constraint(sub_model, - v_ij[i,j] - v[i] + v[j] <= 0)
+                @constraint(sub_model, - v_ij[i,j] + v[i] - v[j] <= 0)
             end
         end
     end
@@ -48,8 +49,9 @@ function subProblem(y_val::Matrix{Float64}, bnd::Int64)::Any
 end
 
 
-function linksBenders(inputFile::String="benders-graphe-hexagone.txt", showResult::Bool= false, timeLimit::Float64=-1.0)::Any
-    readGraph("./tp2/instances/"*inputFile)
+function linksBenders(inputFile::String="benders-graphe-hexagone"; showResult::Bool= false,
+     timeLimit::Float64=-1.0, bnd::Int64=1)::Any
+    readGraph("./tp2/instances/"*inputFile*".txt")
     # Creating the model
     model = Model(CPLEX.Optimizer)
     set_silent(model)
@@ -61,12 +63,13 @@ function linksBenders(inputFile::String="benders-graphe-hexagone.txt", showResul
     @variable(model, y[i in 1:n, j in 1:n] >= 0., Int)
 
     ##### Objective #####
-    @objective(model, Min, sum(y[i,j] for i in 1:n for j in 1:n if adj[i,j] > 0.0))
+    @objective(model, Min, sum(y[i,j] for i in 1:n for j in 1:n if adj[i,j] > 0.0 && i < j))
     
     ##### Constraints #####
 
     hasAddedConstraint = true
     runTime = 0
+    nbIter = 0
     while hasAddedConstraint
         hasAddedConstraint = false
         # Solve current state
@@ -81,17 +84,18 @@ function linksBenders(inputFile::String="benders-graphe-hexagone.txt", showResul
             value = JuMP.objective_value(model)
             # Solve sub problems with current optimum
             y_val = JuMP.value.(y)
-            if showResult && !silent
-                println("Current value ", value, " y ", y_val)
+            if showResult
+                println("Current value ", value)
             end
-            subVal, v_ij_val, v_val, subTime= subProblem(y_val, 1)
+            subVal, v_ij_val, v_val, subTime= subProblem(y_val, bnd)
             runTime += subTime
             if subVal > 1e-5
-                if !silent
+                nbIter += 1
+                if showResult
                     println("Subproblem value ", subVal)
                     println("Adding optimality cut")
                 end
-                @constraint(model, - bnd*sum(y[i,j] * v_ij_val[(i,j)] for i in 1:n for j in 1:n if adj[i,j] > 0.0) + 
+                @constraint(model, - bnd*sum(y[i,j] * v_ij_val[i,j] for i in 1:n for j in 1:n if adj[i,j] > 0.0 && i < j) + 
                 sum(demande[i] * v_val[i] for i in 1:n) <= 0)
                 hasAddedConstraint = true
             end
@@ -115,7 +119,7 @@ function linksBenders(inputFile::String="benders-graphe-hexagone.txt", showResul
             println("Value : ", value, " Time ", runTime, "s.")
         end
 
-        return isOptimal, value, runTime
+        return isOptimal, value, runTime, nbIter
     else
         println("Not feasible!!")
         return
