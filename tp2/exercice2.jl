0de23fec087f2784eda18b5535a22ab454b20f72
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2023-03-10 16:43:40 +0100

    corrected comment

--- tp2/exercice2.jl
+++ tp2/exercice2.jl
@@ -5,7 +5,7 @@ include("./instances/exemple-lecture-graphe.jl")
 """
 include("./tp2/exercice2.jl")
 readGraph("./tp2/instances/benders-graphe-hexagone.txt")
-linksBendersRelaxed("benders-graphe-hexagone.txt")
+linksBendersRelaxed("benders-graphe-hexagone")
 """
 
 function subProblem(y_val::Matrix{Float64}, bnd::Int64)::Any
